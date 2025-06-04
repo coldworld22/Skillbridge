ef303e00d6f8eac01f4aa2151c6337af44de3a24
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-06-04 11:14:14 +0300

    chore: wrap tutorial controller with catchAsync

--- backend/src/modules/users/tutorials/tutorial.controller.js
+++ backend/src/modules/users/tutorials/tutorial.controller.js
@@ -3,8 +3,9 @@ const path = require("path");
 const fs = require("fs");
 const service = require("./tutorial.service");
 const { success } = require("../../../utils/response");
+const catchAsync = require("../../../utils/catchAsync");
 
-exports.createTutorial = async (req, res) => {
+exports.createTutorial = catchAsync(async (req, res) => {
   const data = req.body;
   if (req.files?.thumbnail) {
     data.cover_image = `/uploads/tutorials/${req.files.thumbnail[0].filename}`;
@@ -14,19 +15,19 @@ exports.createTutorial = async (req, res) => {
   }
   const tutorial = await service.createTutorial(data);
   success(res, tutorial);
-};
+});
 
-exports.getAllTutorials = async (req, res) => {
+exports.getAllTutorials = catchAsync(async (req, res) => {
   const tutorials = await service.getAllTutorials(req.query);
   success(res, tutorials);
-};
+});
 
-exports.getTutorialById = async (req, res) => {
+exports.getTutorialById = catchAsync(async (req, res) => {
   const tutorial = await service.getTutorialById(req.params.id);
   success(res, tutorial);
-};
+});
 
-exports.updateTutorial = async (req, res) => {
+exports.updateTutorial = catchAsync(async (req, res) => {
   const data = req.body;
   if (req.files?.thumbnail) {
     data.cover_image = `/uploads/tutorials/${req.files.thumbnail[0].filename}`;
@@ -36,60 +37,60 @@ exports.updateTutorial = async (req, res) => {
   }
   const tutorial = await service.updateTutorial(req.params.id, data);
   success(res, tutorial);
-};
+});
 
-exports.softDeleteTutorial = async (req, res) => {
+exports.softDeleteTutorial = catchAsync(async (req, res) => {
   await service.updateStatus(req.params.id, { status: "archived" });
   success(res, { message: "Archived" });
-};
+});
 
-exports.restoreTutorial = async (req, res) => {
+exports.restoreTutorial = catchAsync(async (req, res) => {
   await service.updateStatus(req.params.id, { status: "draft" });
   success(res, { message: "Restored to draft" });
-};
+});
 
-exports.permanentlyDeleteTutorial = async (req, res) => {
+exports.permanentlyDeleteTutorial = catchAsync(async (req, res) => {
   await service.permanentlyDeleteTutorial(req.params.id);
   success(res, { message: "Permanently deleted" });
-};
+});
 
-exports.togglePublishStatus = async (req, res) => {
+exports.togglePublishStatus = catchAsync(async (req, res) => {
   await service.togglePublishStatus(req.params.id);
   success(res, { message: "Status toggled" });
-};
+});
 
-exports.approveTutorial = async (req, res) => {
+exports.approveTutorial = catchAsync(async (req, res) => {
   await service.updateModeration(req.params.id, "approved");
   success(res, { message: "Tutorial approved" });
-};
+});
 
-exports.rejectTutorial = async (req, res) => {
+exports.rejectTutorial = catchAsync(async (req, res) => {
   await service.updateModeration(req.params.id, "rejected", req.body.reason);
   success(res, { message: "Tutorial rejected" });
-};
+});
 
-exports.bulkApproveTutorials = async (req, res) => {
+exports.bulkApproveTutorials = catchAsync(async (req, res) => {
   await service.bulkUpdateModeration(req.body.ids, "approved");
   success(res, { message: "Bulk approval done" });
-};
+});
 
-exports.bulkTrashTutorials = async (req, res) => {
+exports.bulkTrashTutorials = catchAsync(async (req, res) => {
   await service.bulkUpdateStatus(req.body.ids, "archived");
   success(res, { message: "Bulk archived" });
-};
+});
 
-exports.getFeaturedTutorials = async (req, res) => {
+exports.getFeaturedTutorials = catchAsync(async (req, res) => {
   const featured = await service.getFeaturedTutorials();
   success(res, featured);
-};
+});
 
-exports.getPublishedTutorials = async (req, res) => {
+exports.getPublishedTutorials = catchAsync(async (req, res) => {
   const tutorials = await service.getPublishedTutorials(req.query);
   success(res, tutorials);
-};
+});
 
-exports.getPublicTutorialDetails = async (req, res) => {
+exports.getPublicTutorialDetails = catchAsync(async (req, res) => {
   const tutorial = await service.getPublicTutorialDetails(req.params.id);
   success(res, tutorial);
-};
+});
 
