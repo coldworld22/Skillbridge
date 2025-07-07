3cd3647c6ab8e1442c7e7e4072e21d732ffb346d
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-07-07 15:57:15 +0300

    Fix backend and frontend port configs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,19 +5,19 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '5002',
+        port: '5000',
         pathname: '/uploads/**', // legacy path
       },
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '5002',
+        port: '5000',
         pathname: '/api/uploads/**', // Allow images served via API
       },
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '5002',
+        port: '5000',
         pathname: '/api/uploads/**', // Development server
       },
       {
