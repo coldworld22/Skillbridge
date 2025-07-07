ede7417e446fb0b3a20e00c0d69120e590e67e60
root <root@srv897288.hstgr.cloud>
2025-07-07 06:34:53 +0000

    auth

--- backend/src/migrations/20250612154010_rename_tutorial_tags_to_tags.js
+++
@@ -1,7 +0,0 @@
-exports.up = function(knex) {
-  return knex.schema.renameTable("tutorial_tags", "tags");
-};
-
-exports.down = function(knex) {
-  return knex.schema.renameTable("tags", "tutorial_tags");
-};
