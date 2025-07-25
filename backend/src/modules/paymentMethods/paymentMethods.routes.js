0b55a907ba987a3a580b104ad20fdb48e22735dc
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-07-25 11:06:16 +0300

    Add toast and notifications on payment method creation

--- backend/src/modules/paymentMethods/paymentMethods.routes.js
+++ backend/src/modules/paymentMethods/paymentMethods.routes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./paymentMethods.controller");
 const { verifyToken, isAdmin } = require("../../middleware/auth/authMiddleware");
+const upload = require("./paymentMethodIconUploadMiddleware");
 
 router.use(verifyToken, isAdmin);
 
-router.post("/", controller.createMethod);
+router.post("/", upload.single("icon"), controller.createMethod);
 router.get("/", controller.getMethods);
 router.get("/:id", controller.getMethod);
-router.patch("/:id", controller.updateMethod);
+router.patch("/:id", upload.single("icon"), controller.updateMethod);
 router.delete("/:id", controller.deleteMethod);
 
 module.exports = router;
