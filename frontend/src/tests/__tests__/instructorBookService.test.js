6fedf3c8bc79c4bcb3dcd2889fbf562e15f26dee
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-08-07 08:45:32 +0300

    feat: allow cancelling group join requests

--- frontend/src/tests/__tests__/instructorBookService.test.js
+++ frontend/src/tests/__tests__/instructorBookService.test.js
@@ -21,10 +21,11 @@ beforeEach(() => {
 describe("instructor bookService", () => {
   it("fetches instructor books", async () => {
     const apiData = [{ id: 1 }];
-    api.get.mockResolvedValueOnce({ data: { data: apiData } });
+    const meta = { page: 1 };
+    api.get.mockResolvedValueOnce({ data: { data: apiData, meta } });
     const res = await fetchInstructorBooks();
-    expect(api.get).toHaveBeenCalledWith("/instructor/books");
-    expect(res).toEqual(apiData);
+    expect(api.get).toHaveBeenCalledWith("/instructor/books", { params: {} });
+    expect(res).toEqual({ books: apiData, meta });
   });
 
   it("creates a book", async () => {
