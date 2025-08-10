441b6a652b8255c98a2d4873a9ebf677d61d3d10
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-08-10 16:39:27 +0300

    Validate tutorials before wishlist addition

--- backend/src/modules/users/tutorials/wishlist/tutorialWishlist.service.js
+++ backend/src/modules/users/tutorials/wishlist/tutorialWishlist.service.js
@@ -1,7 +1,15 @@
 const db = require('../../../../config/database');
 const { v4: uuidv4 } = require('uuid');
+const AppError = require('../../../../utils/AppError');
 
 exports.add = async (userId, tutorialId) => {
+  const tutorial = await db('tutorials').where({ id: tutorialId }).first();
+  if (!tutorial) throw new AppError('Tutorial not found', 404);
+  if (tutorial.moderation_status !== 'Approved')
+    throw new AppError('Tutorial not approved', 400);
+  if (tutorial.status !== 'published')
+    throw new AppError('Tutorial not published', 400);
+
   const [item] = await db('tutorial_wishlist')
     .insert({ id: uuidv4(), user_id: userId, tutorial_id: tutorialId })
     .onConflict(['user_id','tutorial_id']).ignore()
