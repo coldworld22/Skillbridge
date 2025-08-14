41d47a614a51e7b52bbf04ef55345dd3ac150a8b
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-08-14 08:33:28 +0300

    test: add book mapping tests

---
+++ frontend/src/utils/bookMapping.js
@@ -0,0 +1,27 @@
+import { buildUrl } from "@/services/bookService";
+
+export const mapBookForCart = (book) => ({
+  id: book.id,
+  name: book.title,
+  author: book.author,
+  category_name: book.category_name,
+  rating: book.rating,
+  price: book.price,
+  cover_url:
+    book.cover_image_url ||
+    buildUrl(book.cover_image) ||
+    "/images/default-book-cover.jpg",
+});
+
+export const mapBookForWishlist = (book) => ({
+  book_id: book.id,
+  title: book.title,
+  author: book.author,
+  category_name: book.category_name,
+  rating: book.rating,
+  price: book.price,
+  cover_url:
+    book.cover_image_url ||
+    buildUrl(book.cover_image) ||
+    "/images/default-book-cover.jpg",
+});
