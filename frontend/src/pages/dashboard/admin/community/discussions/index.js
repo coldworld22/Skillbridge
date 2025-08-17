4f8d9bed316d1326f2d859147bde029529896df4
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-08-17 19:22:12 +0300

    feat: add react hooks import

--- frontend/src/pages/dashboard/admin/community/discussions/index.js
+++ frontend/src/pages/dashboard/admin/community/discussions/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import AdminLayout from "@/components/layouts/AdminLayout";
 import {
