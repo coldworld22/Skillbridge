941db692e16d1a4fbf6de922af48385119eb775e
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-08-22 22:19:04 +0300

    seed permissions without duplicates

---
+++ backend/src/seeds/10_permissions_seed.js
@@ -0,0 +1,11 @@
+exports.seed = async function (knex) {
+  const permission = {
+    code: 'view_course',
+    description: 'Permission to view courses'
+  };
+
+  await knex('permissions')
+    .insert(permission)
+    .onConflict('code')
+    .ignore();
+};
