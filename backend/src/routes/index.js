64b04c25f5ed979931663b5370f480909d6d0283
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-09-07 14:55:29 +0300

    feat: add health check endpoint

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -100,6 +100,7 @@ router.use('/api/library', require('../modules/library/library.routes'));
 router.use('/api/search', require('../modules/search/search.routes'));
 router.use('/api/install', require('../modules/install/install.routes'));
 
+router.use('/api/health', require('./health.routes'));
 router.use('/api/users/classes/lessons', require('./lesson.routes'));
 router.use('/api/video-calls', require('./videoCalls.routes'));
 
