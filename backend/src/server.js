db91a4b752a01fc46d133d2a464388326d5b90cb
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-09-12 18:26:28 +0300

    Handle disabled web installer

--- backend/src/server.js
+++ backend/src/server.js
@@ -187,6 +187,17 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/install", (req, res, next) => {
+  if (!config.ENABLE_INSTALL) {
+    return res
+      .status(410)
+      .send(
+        'The web installer has been disabled. See the <a href="https://github.com/eduskillbridge/SkillBridge/blob/main/docs/installation.md">installation guide</a>.'
+      );
+  }
+  return next();
+});
+
 if (config.ENABLE_INSTALL) {
   const installerPath = path.join(__dirname, "../../install");
   app.use(
