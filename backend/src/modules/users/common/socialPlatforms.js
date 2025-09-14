c564b904b6d0ca7be9a19d6d208e1d4735b4f5a1
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-09-14 15:52:59 +0300

    chore: embed social platforms in backend image

--- backend/src/modules/users/common/socialPlatforms.js
+++ backend/src/modules/users/common/socialPlatforms.js
@@ -1,3 +1,8 @@
-const allowedPlatforms = require("../../../../../shared/socialPlatforms.json");
+let allowedPlatforms;
+try {
+  allowedPlatforms = require("../../../shared/socialPlatforms.json");
+} catch {
+  allowedPlatforms = require("../../../../../shared/socialPlatforms.json");
+}
 
 module.exports = { allowedPlatforms };
