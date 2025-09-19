7097a591b6af45105356ddc69dae69bb14ecc62c
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-09-19 18:55:36 +0300

    Import cache utility in tutorial service

--- backend/src/modules/users/tutorials/tutorial.service.js
+++ backend/src/modules/users/tutorials/tutorial.service.js
@@ -5,6 +5,7 @@ const chapterService = require("./chapters/tutorialChapter.service");
 const { withTransaction } = require("../../../services/transaction.service");
 const { v4: uuidv4 } = require("uuid");
 const slugify = require("slugify");
+const cache = require("../../../utils/cache");
 const { TUTORIAL_STATUS } = require("../../../../shared/tutorialStatus");
 
 exports.createTutorial = async (data, trx = db) => {
