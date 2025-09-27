65d3d29caf4bc1812c217549b4a8b1e49c76f98d
Ayman Mohammed Osman Bashir <154901994+coldworld22@users.noreply.github.com>
2025-09-27 09:21:53 +0300

    feat: include certificate template styling data

--- backend/src/modules/users/tutorials/certificate/certificate.service.js
+++ backend/src/modules/users/tutorials/certificate/certificate.service.js
@@ -1,5 +1,10 @@
 const db = require("../../../../config/database");
 const { v4: uuidv4 } = require("uuid");
+const {
+  TEMPLATE_SELECT_FIELDS,
+  applyTemplateJoin,
+  formatCertificateRow,
+} = require("../../../certificates/certificate.utils");
 
 // Generate a unique certificate code
 const generateCode = () => {
@@ -38,10 +43,27 @@ const isUserCompletedTutorial = async (userId, tutorialId) => {
 };
 
 // Check if certificate already exists
+const buildTemplateAwareQuery = () => {
+  const query = db("certificates");
+  applyTemplateJoin(query);
+  return query.select("certificates.*", ...TEMPLATE_SELECT_FIELDS);
+};
+
 const findExisting = async (userId, tutorialId) => {
-  return await db("certificates")
-    .where({ user_id: userId, tutorial_id: tutorialId })
+  const row = await buildTemplateAwareQuery()
+    .where("certificates.user_id", userId)
+    .where("certificates.tutorial_id", tutorialId)
     .first();
+
+  return formatCertificateRow(row);
+};
+
+const findById = async (id) => {
+  const row = await buildTemplateAwareQuery()
+    .where("certificates.id", id)
+    .first();
+
+  return formatCertificateRow(row);
 };
 
 // Create a new certificate
@@ -64,5 +86,6 @@ module.exports = {
   generateCode,
   isUserCompletedTutorial,
   findExisting,
+  findById,
   issueCertificate,
 };
--- backend/src/modules/users/tutorials/certificate/certificatePublic.controller.js
+++ backend/src/modules/users/tutorials/certificate/certificatePublic.controller.js
@@ -2,22 +2,32 @@ const db = require("../../../../config/database");
 const catchAsync = require("../../../../utils/catchAsync");
 const { sendSuccess } = require("../../../../utils/response");
 const AppError = require("../../../../utils/AppError");
+const {
+  TEMPLATE_SELECT_FIELDS,
+  applyTemplateJoin,
+  formatCertificateRow,
+} = require("../../../certificates/certificate.utils");
 
 exports.verifyByCode = catchAsync(async (req, res) => {
   const { code } = req.params;
 
-  const cert = await db("certificates")
+  const query = db("certificates")
     .leftJoin("users", "users.id", "certificates.user_id")
-    .leftJoin("tutorials", "tutorials.id", "certificates.tutorial_id")
+    .leftJoin("tutorials", "tutorials.id", "certificates.tutorial_id");
+
+  applyTemplateJoin(query);
+
+  const cert = await query
     .select(
       "certificates.*",
       "users.full_name as user_name",
-      "tutorials.title as tutorial_title"
+      "tutorials.title as tutorial_title",
+      ...TEMPLATE_SELECT_FIELDS
     )
     .where("certificates.certificate_code", code)
     .first();
 
   if (!cert) throw new AppError("Certificate not found", 404);
 
-  sendSuccess(res, cert, "Certificate verified");
+  sendSuccess(res, formatCertificateRow(cert), "Certificate verified");
 });
